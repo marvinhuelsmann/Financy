9cc572a399f85a912e7f31d61b432a88564b377c
marvinhuelsmann <mail@marvhuelsmann.com>
2023-03-12 14:36:45 +0100

    update translations, fix products, update version

--- Data/Financy.swift
+++ Data/Financy.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 struct Financy {
-    private var version: String = "0.9.5"
+    private var version: String = "1.0.1"
     private var betaVersion: Bool = true
     
     func getFinancyVersion() -> String {
