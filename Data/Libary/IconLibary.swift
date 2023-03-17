706b569db4472419fc6b54edb7168d35bdee9b68
marvinhuelsmann <mail@marvhuelsmann.com>
2023-03-17 22:29:27 +0100

    add new subscription price, fix some bugs, add new icons to the groups and products, update version (1.0.8)

--- Data/Libary/IconLibary.swift
+++ Data/Libary/IconLibary.swift
@@ -10,11 +10,11 @@ import Foundation
 struct IconLibary {
     
     var avaibleIcons = [
-        "paperplane", "doc", "bolt", "camera", "car", "airplane.departure", "ferry", "scooter", "fuelpump", "house.lodge", "tent.2", "ipad.gen2", "lamp.desk", "popcorn", "chair", "gamecontroller", "carrot", "dollarsign.square", "facemask", "iphone", "macpro.gen3", "macpro.gen2", "figure.wave", "keyboard", "airtag", "display", "macstudio", "applewatch", "airpodsmax", "airpodspro", "homepodmini", "hifispeaker", "appletvremote.gen4", "xmark", "house.fill", "door.left.hand.closed", "balloon.2", "doc.zipper", "tray.2", "externaldrive.badge.wifi", "cursorarrow.rays", "hare", "tortoise.fill", "textformat.size", "hand.point.up.fill", "person.text.rectangle", "fan.ceiling", "shower", "sink"
+        "paperplane", "doc", "bolt", "camera", "car", "airplane.departure", "ferry", "scooter", "fuelpump", "house.lodge", "tent.2", "ipad.gen2", "lamp.desk", "popcorn", "chair", "gamecontroller", "carrot", "dollarsign.square", "facemask", "iphone", "macpro.gen3", "macpro.gen2", "figure.wave", "keyboard", "airtag", "display", "macstudio", "applewatch", "airpodsmax", "airpodspro", "homepodmini", "hifispeaker", "appletvremote.gen4", "xmark", "house.fill", "door.left.hand.closed", "balloon.2", "doc.zipper", "tray.2", "externaldrive.badge.wifi", "cursorarrow.rays", "hare", "tortoise.fill", "textformat.size", "hand.point.up.fill", "person.text.rectangle", "fan.ceiling", "shower", "sink", "shoeprints.fill", "bed.double", "chair.lounge", "pill.fill"
     ]
     
     var avaibleGroupIcons = [
-        "paperplane", "doc", "bolt", "camera", "car", "airplane.departure", "ferry", "scooter", "fuelpump", "house.lodge", "tent.2", "ipad.gen2", "lamp.desk", "popcorn", "chair", "gamecontroller", "carrot", "dollarsign.square", "facemask", "iphone", "shower", "sink",  "tray.2",  "dollarsign.square", "xmark"
+        "paperplane", "doc", "bolt", "camera", "car", "airplane.departure", "ferry", "scooter", "fuelpump", "house.lodge", "tent.2", "ipad.gen2", "lamp.desk", "popcorn", "chair", "gamecontroller", "carrot", "dollarsign.square", "facemask", "iphone", "shower", "sink",  "tray.2",  "dollarsign.square", "xmark", "chair.lounge", "pill.fill", "waveform.path.ecg"
     ]
     
     func getAvaibleProductIcons() -> Array<String> {
